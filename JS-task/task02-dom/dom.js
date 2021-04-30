741ff2e0136ac5020579d1b8b342e1410a6f5b68
Vilhelmus25 <vorvie@gmail.com>
2021-04-30 10:52:37 +0200

    JS ready

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,4 +1,23 @@
-function changeOuterLinks() {}
+function changeOuterLinks() {
+    const links = document.querySelectorAll('nav#link-list a');
+    links.forEach((element) => {
+        if (element.innerHTML.includes('outer-link')) {
+            element.target = '_blank';
+            element.innerHTML = `<strong>${element.innerHTML}</strong>`;
+        }
+    });
 
+    document.querySelectorAll('nav').forEach((element) => {
+        element.style.display = 'flex';
+        element.style.flexDirection = 'column';
+        element.style.margin = '0px auto';
+        element.style.width = '30%';
+        element.style.border = '1px solid blue';
+        element.style.padding = '16px';
+    });
+
+}
+
+changeOuterLinks();
 
 export { changeOuterLinks };
